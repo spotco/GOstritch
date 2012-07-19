59ce94702f5f3f8e29c2dd577857f9f36628c8ba
spotco <mootothemax@gmail.com>
2012-07-18 23:36:00 -0700

    replace icon

--- Classes/GOstrichAppDelegate.h
+++ Classes/GOstrichAppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  GOstrichAppDelegate.h
-//  GOstrich
-//
-//  Created by Pingyang He on 4/2/12.
-//  Copyright University of Washington 2012. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @class RootViewController;
